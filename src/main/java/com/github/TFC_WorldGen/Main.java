983a6e304b13fa15c2ff54f69aef4b407de38507
Skapcz <nitram.t10@gmail.com>
2025-11-07 11:25:30 +0100

    0.0.0 version nothing here

--- src/main/java/com/github/TFC_WorldGen/Main.java
+++ src/main/java/com/github/TFC_WorldGen/Main.java
@@ -1,4 +1,21 @@
 package com.github.TFC_WorldGen;
 
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+
+
+
+@Mod(Main.MOD_ID)
 public class Main {
+    public static final String MOD_ID = "tfc_worldgen";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public Main() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+    }
 }
+
